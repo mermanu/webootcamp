d5f9aaa5866a4bca4c3dde43d9b26381a645f028
mermanu <mmerida.oliveros@gmail.com>
2015-09-20 13:49:10 +0200

    commit

---
+++ src/main/java/com/webootcamp/webapp/wc/render/HelloWorld.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.webootcamp.webapp.wc.render;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class HelloWorld {
+
+    private String firstName = "John";
+    private String lastName = "Doe";
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String showGreeting() {
+        return "Hello" + " " + firstName + " " + lastName + "!";
+    }
+}
